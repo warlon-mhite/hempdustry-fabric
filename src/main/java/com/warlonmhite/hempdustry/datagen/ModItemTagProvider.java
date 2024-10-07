9c6458f6a4fdf964071ce4ddf1b719f70d932858
Warlon Mhite <warlon.doctrinality@simplelogin.fr>
2024-10-07 19:37:50 +0200

    Add hemp armor set

--- src/main/java/com/warlonmhite/hempdustry/datagen/ModItemTagProvider.java
+++ src/main/java/com/warlonmhite/hempdustry/datagen/ModItemTagProvider.java
@@ -5,6 +5,7 @@ import com.warlonmhite.hempdustry.util.ModTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -17,6 +18,12 @@ public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
     protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
         getOrCreateTagBuilder(ModTags.Items.HEMP_SEEDS)
                 .add(ModItems.INDICA_SEEDS);
+
+        getOrCreateTagBuilder(ItemTags.TRIMMABLE_ARMOR)
+            .add(ModItems.HEMP_BEANNIE)
+            .add(ModItems.HEMP_SHIRT)
+            .add(ModItems.HEMP_HAREM_PANTS)
+            .add(ModItems.FLIP_FLOPS);
+        }
     }
 
-}
