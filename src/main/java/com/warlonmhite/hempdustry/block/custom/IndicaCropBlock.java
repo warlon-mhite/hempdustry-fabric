4236a0118eff9fdbae6c5579b5f30cfc4b4333e0
Warlon Mhite <warlon.doctrinality@simplelogin.fr>
2024-10-08 22:58:46 +0200

    Partial fix for IndicaCrop

--- src/main/java/com/warlonmhite/hempdustry/block/custom/IndicaCropBlock.java
+++ src/main/java/com/warlonmhite/hempdustry/block/custom/IndicaCropBlock.java
@@ -1,6 +1,5 @@
 package com.warlonmhite.hempdustry.block.custom;
 
-import com.warlonmhite.hempdustry.block.ModBlocks;
 import com.warlonmhite.hempdustry.item.ModItems;
 import net.minecraft.block.*;
 import net.minecraft.item.ItemConvertible;
@@ -17,12 +16,14 @@ import net.minecraft.world.WorldView;
 public class IndicaCropBlock extends CropBlock {
 
     public static final int FIRST_STAGE_MAX_AGE = 7;
-    public static final int SECOND_STAGE_MAX_AGE = 3;
+    public static final int GROW_UP_STAGE = 3;
+    public static final int SECOND_STAGE_MAX_AGE = 11;
     private static final VoxelShape[] AGE_TO_SHAPE =
             new VoxelShape[]{
                     Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 6.0D, 16.0D),
                     Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 10.0D, 16.0D),
                     Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D),
+                    Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D),
                     Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D),
                     Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D),
                     Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D),
@@ -45,47 +46,51 @@ public class IndicaCropBlock extends CropBlock {
     @Override
     public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
         return super.canPlaceAt(state, world, pos) || (world.getBlockState(pos.down(1)).isOf(this) &&
-                world.getBlockState(pos.down(1)).get(AGE) >= 4 && world.getBlockState(pos.down(1)).get(AGE) <= 7);
+                world.getBlockState(pos.down(1)).get(AGE) > GROW_UP_STAGE && world.getBlockState(pos.down(1)).get(AGE) <= FIRST_STAGE_MAX_AGE);
     }
 
     @Override
     public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
         if (world.getBaseLightLevel(pos, 0) >= 9) {
             int currentAge = this.getAge(state);
-            if (currentAge < this.getMaxAge()) {
+            int secondBlockAge = currentAge - GROW_UP_STAGE;
+            if (currentAge < FIRST_STAGE_MAX_AGE) {
                 float growthChance = getAvailableMoisture(this, world, pos);
-                if (random.nextInt((int)(25.0F / growthChance) + 1) == 0) {
-                    grow(world, pos, state, currentAge);
+                if (random.nextInt((int) (25.0F / growthChance) + 1) == 0) {
+                    if (currentAge == GROW_UP_STAGE && world.getBlockState(pos.up(1)).isAir()) {
+                        world.setBlockState(pos, this.withAge(currentAge + 1), 2);
+                        world.setBlockState(pos.up(1), this.withAge(FIRST_STAGE_MAX_AGE + 1), 2);
+                    }
+                    else if (currentAge > GROW_UP_STAGE && world.getBlockState(pos.up(1)).isOf(this)) {
+                        world.setBlockState(pos, this.withAge(currentAge + 1), 2);
+                        world.setBlockState(pos.up(1), this.withAge(FIRST_STAGE_MAX_AGE + secondBlockAge), 2);
+                    }
+                    else {
+                        world.setBlockState(pos, this.withAge(currentAge + 1), 2);
+                    }
                 }
             }
         }
     }
-
-    private void grow(World world, BlockPos pos, BlockState state, int currentAge) {
-        if (currentAge < FIRST_STAGE_MAX_AGE) {
-            world.setBlockState(pos, this.withAge(currentAge + 1), 2);
-
-            if (currentAge == 4 && world.getBlockState(pos.up()).isAir()) {
-                world.setBlockState(pos.up(), this.withAge(FIRST_STAGE_MAX_AGE + 1), 2);
-            }
-        } else if (currentAge > FIRST_STAGE_MAX_AGE) {
-            // Grow second block (synchronized stages)
-            int secondBlockAge = currentAge - FIRST_STAGE_MAX_AGE; // Age difference between first and second block
-            if (world.getBlockState(pos.down()).isOf(this) && world.getBlockState(pos.down(1)).get(AGE) < 7) {
-                // Update both blocks together (first and second)
-                world.setBlockState(pos.down(),this.withAge(currentAge + 1), 2);
-                world.setBlockState(pos, this.withAge(FIRST_STAGE_MAX_AGE + secondBlockAge), 2); // Grow the first block
-            }
-             // Grow the second block
-        }
-    }
-
+    @Override
     public void applyGrowth(World world, BlockPos pos, BlockState state) {
+        int nextAge = this.getAge(state) + this.getGrowthAmount(world);
+        int maxAge = this.getMaxAge();
+        if (nextAge > maxAge) {
+            nextAge = maxAge;
+        }
         int currentAge = this.getAge(state);
-        int growthAmount = this.getGrowthAmount(world);
-        int newAge = Math.min(currentAge + growthAmount, this.getMaxAge());
+        int secondBlockAge = currentAge - GROW_UP_STAGE;
 
-        grow(world, pos, state, currentAge);
+        if(this.getAge(state) == GROW_UP_STAGE && world.getBlockState(pos.up(1)).isOf(Blocks.AIR)) {
+            world.setBlockState(pos.up(1), this.withAge(FIRST_STAGE_MAX_AGE + 1), 2);
+        }
+        if(this.getAge(state) > GROW_UP_STAGE && world.getBlockState(pos.up(1)).isOf(this)) {
+            world.setBlockState(pos.up(1), this.withAge(FIRST_STAGE_MAX_AGE + secondBlockAge), 2);
+        }
+        else {
+            world.setBlockState(pos, this.withAge(nextAge - SECOND_STAGE_MAX_AGE), 2);
+        }
     }
 
     @Override
