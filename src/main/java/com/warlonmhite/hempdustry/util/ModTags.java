c9d34157304b47d8c5b5476cbfbb36c8c7a8a7ae
Warlon Mhite <warlon.doctrinality@simplelogin.fr>
2026-07-22 01:10:14 +0200

    Remove dead code and placeholder assets

--- src/main/java/com/warlonmhite/hempdustry/util/ModTags.java
+++ src/main/java/com/warlonmhite/hempdustry/util/ModTags.java
@@ -1,7 +1,6 @@
 package com.warlonmhite.hempdustry.util;
 
 import com.warlonmhite.hempdustry.Hempdustry;
-import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
@@ -10,16 +9,9 @@ import net.minecraft.world.biome.Biome;
 
 public class ModTags {
 
-    public static class Blocks {
-
-        private static TagKey<Block> createTag(String name) {
-            return TagKey.of(RegistryKeys.BLOCK, Identifier.of(Hempdustry.MOD_ID, name));
-        }
-    }
-
     public static class Biomes {
 
-        public static final TagKey<Biome> INDICA_FLOWER_GEN = createTag("indicaflowergeneration");
+        public static final TagKey<Biome> INDICA_FLOWER_GEN = createTag("indica_flower_gen");
         private static TagKey<Biome> createTag(String name) {
             return TagKey.of(RegistryKeys.BIOME, Identifier.of(Hempdustry.MOD_ID, name));
         }
