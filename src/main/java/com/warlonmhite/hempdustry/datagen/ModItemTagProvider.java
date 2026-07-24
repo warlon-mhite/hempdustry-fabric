3cbcea1d07f719543b609194f7191970b07582f9
Warlon Mhite <warlon.doctrinality@simplelogin.fr>
2026-07-24 09:27:56 +0200

    Exclude hemp lumber from furnace fuel

--- src/main/java/com/warlonmhite/hempdustry/datagen/ModItemTagProvider.java
+++ src/main/java/com/warlonmhite/hempdustry/datagen/ModItemTagProvider.java
@@ -32,6 +32,22 @@ public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
         getOrCreateTagBuilder(ItemTags.SIGNS).add(ModItems.HEMP_PLANKS_SIGN);
         getOrCreateTagBuilder(ItemTags.HANGING_SIGNS).add(ModItems.HEMP_PLANKS_HANGING_SIGN);
 
+        // Hemp lumber is intentionally fire-immune (like Crimson/Warped nether wood): this is
+        // the item-side "can't be used as furnace fuel" half; the block-side "fire won't spread
+        // to it" half is simply never registering these blocks in FlammableBlockRegistry.
+        getOrCreateTagBuilder(ItemTags.NON_FLAMMABLE_WOOD)
+                .add(ModBlocks.HEMP_PLANKS.asItem())
+                .add(ModBlocks.HEMP_PLANKS_STAIRS.asItem())
+                .add(ModBlocks.HEMP_PLANKS_SLAB.asItem())
+                .add(ModBlocks.HEMP_PLANKS_FENCE.asItem())
+                .add(ModBlocks.HEMP_PLANKS_FENCE_GATE.asItem())
+                .add(ModBlocks.HEMP_PLANKS_DOOR.asItem())
+                .add(ModBlocks.HEMP_PLANKS_TRAPDOOR.asItem())
+                .add(ModBlocks.HEMP_PLANKS_BUTTON.asItem())
+                .add(ModBlocks.HEMP_PLANKS_PRESSURE_PLATE.asItem())
+                .add(ModItems.HEMP_PLANKS_SIGN)
+                .add(ModItems.HEMP_PLANKS_HANGING_SIGN);
+
         // Empty pipe/bong accept Unbreaking/Mending (and enchant at the table). Packed variants
         // inherit any enchantment through the component copy, so they don't need listing here.
         getOrCreateTagBuilder(ItemTags.DURABILITY_ENCHANTABLE)
